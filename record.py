567caf3bba8d029b64d37d8b6b574c8c41313ff1
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-04-10 16:14:50 +0200

    create test record with send and recv on UDP sockets

--- record.py
+++ record.py
@@ -2,13 +2,17 @@ import pyaudio
 import audioop
 from collections import OrderedDict
 
+import time 
+
+
 FORMAT = pyaudio.paInt16
 CHUNK = 1024
-WIDTH = 1
+WIDTH = 4
 CHANNELS = 1
-RATE = 8000
+RATE = 16000
 RECORD_SECONDS = 15
-FACTOR = 2
+FACTOR = 2    
+
 
 p = pyaudio.PyAudio()
 
@@ -23,8 +27,13 @@ print("[*] Recording")
 
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    stream.write(data, CHUNK)
+    new_frames = audioop.lin2ulaw(data, 1)
+    #print(type(data), data)
+    #print(new_frames)
+    stream.write(new_frames, CHUNK)
+    #stream.write(data, CHUNK)
 print("[*] Stop recording")
 
 stream.stop_stream()
 stream.close()
+
---
+++ record_with_send.py
@@ -0,0 +1,72 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 15:20:23 2018
+
+@author: afar
+"""
+
+import pyaudio
+import audioop
+from collections import OrderedDict
+import socket
+import time 
+
+FORMAT = pyaudio.paInt16
+CHUNK = 1024
+WIDTH = 1
+CHANNELS = 1
+RATE = 16000
+RECORD_SECONDS = 15
+FACTOR = 2    
+
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+print("[*] Recording")
+
+####
+
+host = 'localhost'
+port = 50001
+size = 2048
+
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host,port))
+except ConnectionRefusedError as err:
+    print(err)
+    s.close()
+
+
+####
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+   
+    if data:
+        # Write data to pyaudio stream
+        #stream.write(data)  # Stream the recieved audio data
+        try:
+            s.send(data)
+        except ConnectionRefusedError as err:
+            print(err)
+            break
+            
+    
+    # print(type(data), data)
+     
+    #stream.write(data, CHUNK)
+print("[*] Stop recording")
+
+stream.stop_stream()
+stream.close()
+s.close()
