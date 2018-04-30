dc6259465f46c1640effc5dcc22b976dc534f740
jwieczorek <jaroslaw.wieczorek@sealcode.org>
2018-04-30 20:59:33 +0200

    rebuild project directory and add virtualenv

---
+++ JaroEliCall/src/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 17 23:56:59 2018
+@author: afar
+"""
+
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "guifoo",
+        version = "0.1",
+        description = "My GUI application!",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("Klient.py", base=base)])
--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 17 23:56:59 2018
-@author: afar
-"""
+from setuptools import setup
 
-import sys
-from cx_Freeze import setup, Executable
+try:
+    from pyqt_distutils.build_ui import build_ui
+    cmdclass = {"build_ui": build_ui}
+except ImportError:
+    cmdclass = {}
 
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+setup(
+    name="JaroEliCall",
+    version="0.1",
+    packages=["JaroEliCall"],
+    cmdclass=cmdclass,
+)
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
 
-setup(  name = "guifoo",
-        version = "0.1",
-        description = "My GUI application!",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("Klient.py", base=base)])
