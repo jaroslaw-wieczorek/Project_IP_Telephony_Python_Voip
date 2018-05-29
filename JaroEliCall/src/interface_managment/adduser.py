74bfa0b6feb6721fb2168e4f57ac1059e923facf
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-05-29 21:15:39 +0200

    merge

--- JaroEliCall/src/interface_managment/adduser.py
+++ JaroEliCall/src/interface_managment/adduser.py
@@ -9,7 +9,7 @@ Created on Sat May 26 13:42:22 2018
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtWidgets import QTableWidget
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 
 import os
 import sys
@@ -22,31 +22,60 @@ from gui.resources import zasoby_rc
 
 from gui.adduser_ui import Ui_FormInterface
 
+
 class AdduserDialog(QDialog, Ui_FormInterface):
+    
     def __init__(self):
         super(AdduserDialog, self).__init__()
         icon = QIcon()
         icon.addPixmap(QPixmap(":/icons/icons/111317-essential-ui/png/profile-user-1.png"), QIcon.Active, QIcon.Off)
         self.setupUi(self)
         
+        
     def set_push_button_logout(self, funct):
-        self.push_button_logout.clicked.connect(funct)
+        self.push_button_logout.clicked.connect(funct
+                                                
+                                                )
     def set_push_button_call(self, funct):
-        self.push_button_call.clicked.connect(funct)       
+        self.push_button_call.clicked.connect(funct)    
+        
+        
     def set_push_button_invite(self, funct):
         self.push_button_invite.clicked.connect(funct)
+        
         
     def set_avatar(self, pic):
         self.label_avatar.setPixmap(QPixmap(str(pic)))
     
+    
     def set_fit_width(self):
-        self.tab_widget_list_of_users.horizontalHeader().setStretchLastSection(True);
+        self.table_widget_list_of_users.horizontalHeader().setStretchLastSection(True);
+        
+        
+    def add_item_to_list_of_users(self, row, col, item: QTableWidgetItem):
+        self.table_widget_list_of_users.setItem(row, col, item)
+        
+        
+    def add_row_to_list_of_users(self, item):
+        newRowNum = self.table_widget_list_of_users.rowCount()
+        self.table_widget_list_of_users.insertRow(newRowNum)
+        for cell, value in enumerate(item):
+            self.table_widget_list_of_users.setItem(newRowNum, cell, QTableWidgetItem(str(value)))
+        
+        
+    def test_add(self):
+        rowPosition = self.table_widget_list_of_users.rowCount()
+        print(rowPosition)
+        self.table_widget_list_of_users.insertRow(rowPosition)
+        self.table_widget_list_of_users.setItem(rowPosition, 0, QTableWidgetItem("text2"))
+        self.table_widget_list_of_users.setItem(rowPosition, 2, QTableWidgetItem("text3"))
+        
         
     def nothing(self):
         print("Do nothing!")
         
         
-"""
+
 #Fot tests
 
 if __name__  == '__main__':
@@ -56,6 +85,8 @@ if __name__  == '__main__':
     window.set_push_button_call(window.nothing)
     window.set_push_button_invite(window.nothing)
     window.set_fit_width()
+    window.add_row_to_list_of_users(["jaro", 77,"avatar"])
+    
+    #window.test_add()
     window.show()
     sys.exit(app.exec_())
-"""
